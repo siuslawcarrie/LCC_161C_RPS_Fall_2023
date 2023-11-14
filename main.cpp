c0afc68ed673f44fe567c8427415c0adc6c04e44
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2023-11-14 14:34:48 -0800

    2 x .cpp + .h, runtime errors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "rps_functions.h"
+#include "rps_functions.cpp"
 
 using namespace std;
 
@@ -9,11 +10,12 @@ int main(){
     int player_score;
     int computer_score;
     int tie_score;
-    display_instructions();
-    player_move = player_move();
-    computer_move = computer_move();
-    determine_winner(player_move, computer_move, &player_score, computer_score, &tie_score);
-    char get_do_again();
+   display_instructions();
+    do {
+        player_move = player_move_func();
+        computer_move = computer_move_func();
+        determine_winner(player_move, computer_move, player_score, computer_score, tie_score);
+    } while (get_do_again());
 
     return 0;
 }
---
+++ rps_functions.cpp
@@ -0,0 +1,120 @@
+#include "rps_functions.h"
+
+//function display instructions
+void display_instructions()
+{
+    cout << "Welcome to the game of rock, paper, scissors. To play, each participant at the same time chooses ";
+    cout << "rock, paper, or scissors. ";
+    cout << "The winner is determined thusly: scissors cut paper, paper covers rock or rock smashes scissors." << endl;
+}
+
+//function get player move
+char player_move()
+{
+    char player_move;
+
+    do
+    {
+        cout << "Please enter r for rock, p for  `paper, s for scissors: " << endl;
+        cin >> player_move;
+        // clear any error flags on input
+        cin.clear();
+        // flush the buffer
+        cin.ignore(100, '\n');
+        player_move = tolower(player_move);
+        if (player_move != 'r' && player_move != 'p' && player_move != 's')
+            cout << "Sorry, that's not a valid entry. Please enter r for rock, p for  `paper, s for scissors: " << endl;
+    } while (player_move != 'r' && player_move != 'p' && player_move != 's');
+    return player_move;
+}
+//function get computer move
+
+char computer_move()
+{
+    int computer_move = rand() % 3 + 1;
+    if (computer_move == 1)
+    {
+        return 'r';
+    } else if (computer_move == 2)
+    {
+        return 'p';
+    } else
+    {
+        return 's';
+    }
+}
+
+//function play again
+bool get_do_again()
+{
+    char playAgain;
+    do
+    {
+        cout << "Do you want to play again? Enter y for yes or n for no ";
+        cin >> playAgain;
+        // clear any error flags on input
+        cin.clear();
+        // flush the buffer
+        cin.ignore(100, '\n');
+        playAgain = tolower(playAgain);
+        if (playAgain != 'y' && playAgain != 'n')
+            cout << "Sorry, that's not a valid entry. Please enter y or n\n";
+    } while (playAgain != 'y' && playAgain != 'n');
+
+
+    cout << endl;
+
+    switch (playAgain)
+    {
+        case 'y':
+            return true;
+        case 'n':
+            return false;
+    }
+}
+
+//winner function
+void determine_winner(char player_move, char computer_move, int &player_score, int &computer_score, int &tie_score) {
+    if (player_move == 'r' && computer_move == 'p') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Computer wins!" << endl;
+        computer_score++;
+    }
+    else if (player_move == 'r' && computer_move == 's') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << " Player wins!" << endl;
+        player_score++;
+    }
+    else if (player_move == 'p' && computer_move == 'r') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Player wins! " << endl;
+        player_score++;
+    }
+    else if (player_move == 'p' && computer_move == 's') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Computer wins!" << endl;
+        computer_score++;
+    }
+    else if (player_move == 's' && computer_move == 'r') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Computer wins!" << endl;
+        computer_score++;
+    }
+    else if (player_move == 's' && computer_move == 'p') {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Player wins!" << endl;
+        player_score++;
+    }
+    else {
+        cout << "Player chose " << player_move << endl;
+        cout << "Computer chose " << computer_move << endl;
+        cout << "Tie!" << endl;
+        tie_score++;
+    }
+}
